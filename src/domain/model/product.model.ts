8022ac4cefff30c97755859290ac4b8da3e5ed2a
Hernan Velasquez <velasquezmhernan@gmail.com>
2023-04-11 09:18:06 -0500

    feat: Creando estructura proyecto
    Realizando la creacion del proyecto y generado la estructura de domain, application, infrastructure y creado el modelo

---
+++ src/domain/model/product.model.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid } from 'uuid';
+
+export class ProductDomain {
+  id: string;
+  name: string;
+  inventory: string;
+  enabled: boolean;
+  min: number;
+  max: number;
+
+  constructor({
+    name,
+    inventory,
+    enabled,
+    min,
+    max,
+  }: {
+    name: string;
+    inventory: string;
+    enabled: boolean;
+    min: number;
+    max: number;
+  }) {
+    this.id = uuid();
+    this.name = name;
+    this.inventory = inventory;
+    this.enabled = enabled;
+    this.min = min;
+    this.max = max;
+  }
+}
---
+++ src/domain/repositories/product.repository.ts
@@ -0,0 +1,11 @@
+import { ProductDomain } from '../model/product.model';
+
+export interface IProductRepository<
+  Model extends ProductDomain = ProductDomain,
+> {
+  findAllProduct(): Promise<Model[]>;
+  findProductById(id: string): Promise<Model>;
+  registerProduct(product: Model): Promise<Model>;
+  updateProduct(id: string, product: Model): Promise<Model>;
+  deleteProduct(id: string): Promise<boolean>;
+}
